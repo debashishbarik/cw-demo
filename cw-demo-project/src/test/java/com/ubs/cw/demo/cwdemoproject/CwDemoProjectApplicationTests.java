1770340f34fc18831813672ce85850624450c901
Barik <debashish.barik@accenture.com>
2019-11-27 22:22:10 +0530

    Initial

---
+++ cw-demo-project/src/test/java/com/ubs/cw/demo/cwdemoproject/CwDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubs.cw.demo.cwdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
