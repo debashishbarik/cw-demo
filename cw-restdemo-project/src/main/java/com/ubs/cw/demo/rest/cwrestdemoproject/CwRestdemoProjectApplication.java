5f2128d505181b71cd5cb51200250d566140ed75
Barik <debashish.barik@accenture.com>
2019-11-28 01:14:51 +0530

    cw rest demo

---
+++ cw-restdemo-project/src/main/java/com/ubs/cw/demo/rest/cwrestdemoproject/CwRestdemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ubs.cw.demo.rest.cwrestdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwRestdemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwRestdemoProjectApplication.class, args);
+	}
+
+}
