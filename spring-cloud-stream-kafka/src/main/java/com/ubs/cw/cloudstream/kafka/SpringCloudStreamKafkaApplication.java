d957c9573ddaa6e8c5f2a9c04db9d905de946c87
Barik <debashish.barik@accenture.com>
2019-12-18 23:02:11 +0530

    Logging enabled

--- spring-cloud-stream-kafka/src/main/java/com/ubs/cw/cloudstream/kafka/SpringCloudStreamKafkaApplication.java
+++ spring-cloud-stream-kafka/src/main/java/com/ubs/cw/cloudstream/kafka/SpringCloudStreamKafkaApplication.java
@@ -3,10 +3,13 @@ package com.ubs.cw.cloudstream.kafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class SpringCloudStreamKafkaApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(SpringCloudStreamKafkaApplication.class, args);
 	}
 
