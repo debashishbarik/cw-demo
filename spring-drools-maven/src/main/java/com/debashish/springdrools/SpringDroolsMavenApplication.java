92583df6b51df8cd6c66ee423e12ebf2fef4f45c
Barik <debashish.barik@accenture.com>
2020-02-23 17:54:44 +0530

    drool spring boot initial

---
+++ spring-drools-maven/src/main/java/com/debashish/springdrools/SpringDroolsMavenApplication.java
@@ -0,0 +1,13 @@
+package com.debashish.springdrools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDroolsMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDroolsMavenApplication.class, args);
+	}
+
+}
